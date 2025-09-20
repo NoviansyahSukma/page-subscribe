cb4c84c0eacc093d5188597faf5cc931225509ed
Noviansyah <cymbalavedis@gmail.com>
2025-09-20 21:35:24 +0700

    thirt commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/page-subscribe/",
   plugins: [react(), tailwindcss()],
+  base: "/page-subscribe/",
 });
